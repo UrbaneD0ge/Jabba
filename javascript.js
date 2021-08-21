37316b5bc3d45f0f71b0045150b602628fba10f0
Kip <88916382+UrbaneD0ge@users.noreply.github.com>
2021-08-21 13:25:45 -0400

    commit on 'main'

---
+++ javascript.js
@@ -0,0 +1,9 @@
+    document.getElementById("button1").addEventListener("click", function(){
+    document.getElementById("box").div.style.height= "300px";
+});
+document.getElementById("button2").addEventListener("click", function(){
+    document.getElementById("box").div.style.backgroundColor="blue"
+});
+    document.getElementById("button3").addEventListener("click", function(){
+       .div.style.opacity="50%"
+    });
